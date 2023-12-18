c900356b03c9eb2ae5ec610598139a13febb6bf7
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-12-18 09:39:53 +0100

    update js cars

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,20 +1,58 @@
 // write cool JS here!
 
-class testObject {
-    constructor(msg) {
-        console.log("Hello from object " + msg);
+// class testObject {
+//     constructor(msg) {
+//         console.log("Hello from object " + msg);
 
-        this.message=msg;
-        console.log(this.message);    }
+//         this.message=msg;
+//         console.log(this.message);    }
+// }
+
+// const myObject = new testObject("objekt 1");
+// const myObject2 = new testObject("objekt 2");
+
+
+// myObject2.message = "my message";
+// myObject.message = "juletræ";
+
+
+// console.log(myObject2.message);
+// console.log(myObject.message);
+
+// Some more js stuff
+
+class car {
+
+    constructor(make, model, year, condition) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.condition = condition;
+    }
+
+    getInfo() {
+        // console.log(`jeg er en ${this.make}, model: ${this.model} fra ${this.year} jeg er i ${this.condition}`);
+        return (`jeg er en ${this.make}, model: ${this.model} fra ${this.year} jeg er i ${this.condition} stand`);
+    }
+
+    getSumary() {
+
+        return `${this.make}, model: ${this.model}`;
+    }
 }
 
-const myObject = new testObject("objekt 1");
-const myObject2 = new testObject("objekt 2");
+const myObject = new car('volvo', 'v40', 2001, 'fin');
+const myObject2 = new car('nissan', 'z', 2001, 'lorte');
 
+console.log(myObject.getInfo());
+console.log(myObject.getSumary());
+console.log(myObject2.getInfo());
 
-myObject2.message = "my message";
-myObject.message = "juletræ";
+const myCars = [];
+myCars.push(new car('volvo', 'v40', 2001, 'fin'));
+myCars.push(new car('nissan', 'z', 2001, 'lorte'));
 
+console.log(myCars);
 
-console.log(myObject2.message);
-console.log(myObject.message);
+console.log(myCars[0].getInfo());
+console.log(myCars[0].make = 'John Deer');
